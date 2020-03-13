db328917f5370a2c4a07f639879e904a2fe7bae4
Reagan Barrington <reagan@barringtonfamily.net>
2020-03-13 11:17:46 -0400

    axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
+class App extends React.Component {
+  state = {
+    data: [],
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App here</h1>
-    </div>
-  );
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/players')
+      .then(res => {
+        this.setState({
+          data: [...this.state.data, res.data],
+        });
+      })
+      .catch(err => console.log(err.message));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>App here</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import App from './App'; // class component
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
